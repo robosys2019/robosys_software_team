10be3719874dfd4dd27c0cc74da377b2c98e776f
cweiss <charlievweiss@gmail.com>
2019-03-31 18:44:23 -0400

    moved files out of path_planning folder

--- opencv_map_maker.py
+++ opencv_map_maker.py
@@ -9,7 +9,7 @@ import seaborn as sns
 
 class Map_Maker():
     def __init__(self):
-        self.data = pd.read_csv('example_map.txt', sep=", |;", header=None, engine='python')
+        self.data = pd.read_csv('mat.txt', sep=", |;", header=None, engine='python')
         #print(self.data)
         self.data = self.data.as_matrix() # convert to numpy array
         #print(self.data)

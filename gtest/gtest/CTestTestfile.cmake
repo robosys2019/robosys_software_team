f98dcd18bbd9972422341739ec51687afdf6a49e
Filipe Borba <filipefborba@gmail.com>
2019-04-25 16:34:06 -0400

    removed junk files

--- gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/charlie/catkin_ws/src/robosys_software_team/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
